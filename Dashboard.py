c0bd0ce13dfb5be0a28e5504f0242cbefbd4f266
Rodrigo <rleitaopb@gmail.com>
2024-02-02 15:16:29 -0300

    simple authentication and order history

--- Dashboard.py
+++ Dashboard.py
@@ -1,11 +1,35 @@
 import streamlit as st
+import streamlit_authenticator as stauth
 
 st.set_page_config(
   page_title='Trading App',
   layout='wide',
-  initial_sidebar_state='collapsed',
+  page_icon="chart_with_upwards_trend",
+  initial_sidebar_state='expanded',
 )
 
-titulo_style = "font-size: 32px; font-weight:bold; text-align:center;"
+st.title('Trading App')
 
-st.markdown("<h1 style='{}'>Trading App".format(titulo_style), unsafe_allow_html=True)
+import yaml
+from yaml.loader import SafeLoader
+with open('./config.yaml') as file:
+  config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized'],
+)
+
+authenticator.login()
+
+if st.session_state["authentication_status"]:
+    authenticator.logout()
+    st.write(f'Welcome *{st.session_state["name"]}*')
+    st.title('Dashboard')
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
--- pages/History.py
+++ pages/History.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import pandas as pd
 
-titulo_style = "font-size: 32px; font-weight:bold; text-align:center;"
+if not st.session_state.authentication_status:
+    st.info('Please Login from the Home page and try again.')
+    st.stop()
+else:
+  st.title('Order History')
 
-st.markdown("<h1 style='{}'>History".format(titulo_style), unsafe_allow_html=True)
-
-order_history = pd.read_csv('./data/transactions.csv')
-st.dataframe(order_history)
+  order_history = pd.read_csv('./data/transactions.csv')
+  st.dataframe(order_history)
